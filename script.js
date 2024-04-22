dc3862ff4cc9df5cadccda66f6a85d47b4fb746a
rankexet <163413334+rankexet@users.noreply.github.com>
2024-04-22 10:25:36 +0200

    JS media queries

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ var blurredElement = document.getElementById("manifest");   // store element for
 function mouseStopped(){                                 // the actual function that is called
     blurredElement.classList.remove("blur");
 }
-
-window.addEventListener("mousemove", () => {
-    blurredElement.classList.add("blur")
-    clearTimeout(timer);
-    timer = setTimeout(mouseStopped,500);
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        blurredElement.classList.add("blur");
+        setTimeout(() => {
+            blurredElement.classList.remove("blur");
+        }, 2000);
+    } else {
+        blurredElement.classList.remove("blur");
+        window.addEventListener("mousemove", () => {
+            blurredElement.classList.add("blur");
+            clearTimeout(timer);
+            timer = setTimeout(mouseStopped,500);
+        });
+    }
+}
+  
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 700px)");
+  
+// Call listener function at run time
+myFunction(x);
+  
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+    myFunction(x);
 });
-
-window.addEventListener("devicemotion", (event) => {
-    blurredElement.textContent = "Skak";
-  });
